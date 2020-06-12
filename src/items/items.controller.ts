c5aeea1fd3e26c2d9e0c44b2393acff35b61dd6a
olympia <owojcik7@gmail.com>
2020-06-11 20:02:11 -0400

    Generate items controller and implement Get and Post methods

---
+++ src/items/items.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item-dto'
+
+@Controller('items')
+export class ItemsController {
+    @Get()
+    findAll(): string {
+        return `Get all items`
+    }
+
+
+    @Post()
+    // We're putting in our CreateItemDto - now we have access to createItemDto inside the function
+    // e.g. createItemDto.name, createItemDto.quantity
+    create(@Body() createItemDto: CreateItemDto): string {
+        return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`
+    }
+
+
+}
